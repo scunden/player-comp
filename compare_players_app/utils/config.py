0130e255902d96ea16ef6ffbfe24e265bc797c5a
Steven Cunden <scunden3@gatech.edu>
2024-04-26 13:53:01 -0400

    Deleted notebook

---
+++ compare_players_app/utils/config.py
@@ -0,0 +1,4 @@
+PLAYERS_DIR_URL = 'https://fbref.com/en/players/'
+PLAYERS_DIR_UL_CLASS = 'page_index'
+PLAYERS_DIR_DIV_CLASS = 'section_content'
+PLAYERS_DIR_STORE_PATH = '../data/raw/players_url.pickle'
